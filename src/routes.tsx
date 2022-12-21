76190fa0d1ea8345c8d50f749e54f78f88870ee9
farjadakbar4 <farjadakbar4@gmail.com>
2022-12-21 21:06:22 +0500

    update

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,29 @@
-import {
-  Routes as Router, Route, BrowserRouter,
-} from "react-router-dom";
+/** @format */
+
+import { Routes as Router, Route, BrowserRouter } from "react-router-dom";
 import Layout from "./submodule/components/admin/Layout/Layout";
 import OrganizationPage from "./pages/OrganizationPage/OrganizationPage";
 import OrganizationDetailPage from "./pages/OrganizationDetailPage/OrganizationDetailPage";
 import OrganizationDiscountPage from "./pages/OrganizationDiscountPage/OrganizationDiscountPage";
 import NotfoundPage from "./submodule/pages/NotFoundPage/NotfoundPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Router>
-        <Route path='*' element={<NotfoundPage />} />
+        <Route path="*" element={<NotfoundPage />} />
+        <Route path="/login" element={<LoginPage userType="admin" />} />
         <Route path="/" element={<Layout />}>
           <Route path="organization" element={<OrganizationPage />} />
-          <Route path="organization/detail/:id" element={<OrganizationDetailPage />} />
-          <Route path="organization/discount/:organizationDetailId" element={<OrganizationDiscountPage />} />
+          <Route
+            path="organization/detail/:id"
+            element={<OrganizationDetailPage />}
+          />
+          <Route
+            path="organization/discount/:organizationDetailId"
+            element={<OrganizationDiscountPage />}
+          />
         </Route>
       </Router>
     </BrowserRouter>
